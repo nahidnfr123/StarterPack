0df32e95475dac0b4500b953ddedbed8b93eeafb
nahidnfr123 <nahidferdous240@gmail.com>
2023-02-14 14:21:35 +0600

    Api Projects Added

---
+++ laravel-passport-api/routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('guest')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+});
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('email/verification-notification', [AuthController::class, 'sendEmailVerificationNotification']);
+
+    Route::get('/user', [UserController::class, 'me']);
+    Route::get('/users', [UserController::class, 'getAllUsers']);
+});
+
