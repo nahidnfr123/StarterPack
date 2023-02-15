e87ed3a8a15c5b19ba457aee59b3e0814fb275b1
nahidnfr123 <nahidferdous240@gmail.com>
2023-02-16 01:40:37 +0600

    Edit Profile Validation added

--- react/src/pages/auth/Register.jsx
+++ react/src/pages/auth/Register.jsx
@@ -27,9 +27,9 @@ export default function SignIn() {
 
   const ValidationSchema = Yup.object().shape({
     name: Yup.string().required('Required').min(2, 'Too Short!').max(50, 'Too Long!'),
-    email: Yup.string().email('Invalid email').required('Required'),
-    password: Yup.string().notRequired().min(6, 'Too Short!').max(60, 'Too Long!'),
-    password_confirmation: Yup.string().notRequired().min(6, 'Too Short!').max(60, 'Too Long!'),
+    email: Yup.string().required('Required').email('Invalid email'),
+    password: Yup.string().required('Required').min(6, 'Too Short!').max(60, 'Too Long!'),
+    password_confirmation: Yup.string().required('Required').min(6, 'Too Short!').max(60, 'Too Long!'),
   });
 
   const handleSubmit = async (values, props) => {
@@ -65,97 +65,93 @@ export default function SignIn() {
         <Avatar sx={{m: 2, bgcolor: 'secondary.main'}}>
           <LockOutlinedIcon/>
         </Avatar>
-        <Typography component="h1" variant="h5">
-          Register
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 1}}>
-          <Formik
-            initialValues={initialValues}
-            validationSchema={ValidationSchema}
-            onSubmit={handleSubmit}
-          >
-            {({
-                values,
-                errors,
-                touched,
-                handleChange,
-                handleBlur,
-                handleSubmit,
-                isSubmitting,
-                /* and other goodies */
-              }) => (
-              <Form>
-                <Field
-                  as={TextField}
-                  error={!!errors.name}
-                  margin="normal"
-                  fullWidth
-                  id="name"
-                  label="Name"
-                  name="name"
-                  autoComplete=""
-                  autoFocus
-                  helperText={<ErrorMessage name='name'/>}
-                />
-                <Field
-                  as={TextField}
-                  error={!!errors.email}
-                  margin="normal"
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  type="email"
-                  helperText={<ErrorMessage name='email'/>}
-                />
-                <Field
-                  as={TextField}
-                  error={!!errors.password}
-                  margin="normal"
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="password"
-                  // validate={validatePassword}
-                  helperText={<ErrorMessage name='password'/>}
-                />
-                <Field
-                  as={TextField}
-                  error={!!errors.password_confirmation}
-                  margin="normal"
-                  fullWidth
-                  name="password_confirmation"
-                  label="Password Confirmation"
-                  type="password"
-                  id="passwordConfirmation"
-                  autoComplete=""
-                  helperText={<ErrorMessage name='password_confirmation'/>}
-                />
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  sx={{mt: 3, mb: 2}}
-                  disabled={isSubmitting}
-                >
-                  Register
-                </Button>
-              </Form>
-            )}
-          </Formik>
-          <Grid container>
-            <Grid item xs>
-            </Grid>
-            <Grid item>
-              <Link to="/auth/login" variant="body2">
-                {"Already have an account? Login"}
-              </Link>
-            </Grid>
+        <Typography component="h1" variant="h5">Register</Typography>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={ValidationSchema}
+          onSubmit={handleSubmit}
+        >
+          {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+              /* and other goodies */
+            }) => (
+            <Form>
+              <Field
+                as={TextField}
+                error={!!errors.name}
+                margin="normal"
+                fullWidth
+                id="name"
+                label="Name"
+                name="name"
+                autoComplete=""
+                autoFocus
+                helperText={<ErrorMessage name='name'/>}
+              />
+              <Field
+                as={TextField}
+                error={!!errors.email}
+                margin="normal"
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                type="email"
+                helperText={<ErrorMessage name='email'/>}
+              />
+              <Field
+                as={TextField}
+                error={!!errors.password}
+                margin="normal"
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="password"
+                // validate={validatePassword}
+                helperText={<ErrorMessage name='password'/>}
+              />
+              <Field
+                as={TextField}
+                error={!!errors.password_confirmation}
+                margin="normal"
+                fullWidth
+                name="password_confirmation"
+                label="Password Confirmation"
+                type="password"
+                id="passwordConfirmation"
+                autoComplete=""
+                helperText={<ErrorMessage name='password_confirmation'/>}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{mt: 3, mb: 2}}
+                disabled={isSubmitting}
+              >
+                Register
+              </Button>
+            </Form>
+          )}
+        </Formik>
+        <Grid container>
+          <Grid item xs>
           </Grid>
-        </Box>
+          <Grid item>
+            <Link to="/auth/login" variant="body2">
+              {"Already have an account? Login"}
+            </Link>
+          </Grid>
+        </Grid>
       </Box>
       {/*<Copyright sx={{mt: 8, mb: 4}}/>*/}
     </>
