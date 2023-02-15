d1450b652fbcd1778c01bb2369af0f62c67ff3cd
nahidnfr123 <nahidferdous240@gmail.com>
2023-02-15 13:29:32 +0600

    Removed Un Usual things From Sanctum Api.

--- laravel-9-sanctum-api/routes/api.php
+++ laravel-9-sanctum-api/routes/api.php
@@ -1,17 +1,16 @@
 <?php
 
-use App\Http\Controllers\AccountController;
 use App\Http\Controllers\AuthenticationController;
-use App\Http\Controllers\TransactionCategoryController;
-use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthenticationController::class, 'register']);
-Route::post('/login', [AuthenticationController::class, 'login']);
+Route::group(['middleware' => ['throttle:5,1']], function () {
+    Route::post('/register', [AuthenticationController::class, 'register']);
+    Route::post('/login', [AuthenticationController::class, 'login']);
+});
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::get('/user', function(Request $request) {
+    Route::get('/user', function (Request $request) {
         return $request->user();
     });
     Route::post('/logout', [AuthenticationController::class, 'logout']);
