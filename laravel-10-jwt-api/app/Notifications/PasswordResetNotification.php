1711a4432740ebc14194127d429313285008985a
Nahid Ferdous <65497148+nahidnfr123@users.noreply.github.com>
2023-02-19 03:22:07 +0600

    Passport Project Init

--- laravel-10-jwt-api/app/Notifications/PasswordResetNotification.php
+++ laravel-10-jwt-api/app/Notifications/PasswordResetNotification.php
@@ -11,51 +11,41 @@ class PasswordResetNotification extends Notification
 {
     use Queueable;
 
-    public $data;
-
     /**
      * Create a new notification instance.
-     *
-     * @return void
      */
-    public function __construct($data)
+    public function __construct()
     {
-        $this->data = $data;
+        //
     }
 
     /**
      * Get the notification's delivery channels.
      *
-     * @param mixed $notifiable
-     * @return array
+     * @return array<int, string>
      */
-    public function via(mixed $notifiable)
+    public function via(object $notifiable): array
     {
         return ['mail'];
     }
 
     /**
      * Get the mail representation of the notification.
-     *
-     * @param mixed $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
      */
-    public function toMail($notifiable)
+    public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
-            ->line('You are receiving this email because we received a password reset request for your account.')
-            ->action('Reset Password', $this->data['password_reset_link'])
-            ->line('This password reset link will expire in 60 minutes.')
-            ->line('If you did not request a password reset, no further action is required.');
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
     }
 
     /**
      * Get the array representation of the notification.
      *
-     * @param mixed $notifiable
-     * @return array
+     * @return array<string, mixed>
      */
-    public function toArray($notifiable)
+    public function toArray(object $notifiable): array
     {
         return [
             //
