4bfc0fb7278d890b22ccd4a5f3765efc0c3bb8a9
Nahid Ferdous <65497148+nahidnfr123@users.noreply.github.com>
2023-02-19 11:05:33 +0600

    Passport api initial setup

--- laravel-10-passport-api/routes/api.php
+++ laravel-10-passport-api/routes/api.php
@@ -1,19 +1,21 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::group(['middleware' => ['throttle:5,1', 'guest']], function () {
+    Route::post('/register', [AuthenticationController::class, 'register']);
+    Route::post('/login', [AuthenticationController::class, 'login']);
+    Route::post('/send-otp', [UserController::class, 'sendOtp']);
+    Route::put('/reset-password', [UserController::class, 'resetPassword']);
+});
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::put('/user', [UserController::class, 'update']);
+    Route::post('/logout', [AuthenticationController::class, 'logout']);
 });
