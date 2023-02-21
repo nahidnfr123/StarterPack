e0aae97a4202acaa782e041f55bd7dc1eb53fe97
Nahid Ferdous <65497148+nahidnfr123@users.noreply.github.com>
2023-02-22 01:13:35 +0600

    Login And Register Done

---
+++ nuxt3-custom-auth/src/layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="min-h-screen">
+    <TheHeader/>
+    <main class="min-h-[70vh] mb-10">
+      <slot></slot>
+    </main>
+    {{ authStore.user }}
+    <TheFooter/>
+  </main>
+</template>
+
+<script setup lang="ts">
+import TheHeader from "~/components/common/TheHeader.vue";
+import TheFooter from "~/components/common/TheFooter.vue";
+
+import {useAuthStore} from "~/stores/auth";
+
+const authStore = useAuthStore()
+authStore.getUser()
+</script>
---
+++ nuxt3-custom-auth/src/pages/auth/forget-password.vue
@@ -0,0 +1,9 @@
+<template>
+  forget-password
+</template>
+
+<script setup>
+definePageMeta({
+  layout: 'auth'
+})
+</script>
