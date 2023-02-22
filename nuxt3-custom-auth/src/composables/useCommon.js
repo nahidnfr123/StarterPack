a764f3b8d9a9943a2b543eb665e2466854da145a
Nahid Ferdous <65497148+nahidnfr123@users.noreply.github.com>
2023-02-22 16:25:53 +0600

    Forms Update

--- nuxt3-custom-auth/src/composables/useCommon.js
+++ nuxt3-custom-auth/src/composables/useCommon.js
@@ -8,6 +8,7 @@ export const redirectTo = (path) => {
 }
 
 export const throwFormError = (error, formNode) => {
+  if (!error || !formNode) return
   if (error?.status === 422) formNode.setErrors(error?.data?.errors) // Validation Error ...
   else if (error?.status) formNode.setErrors('Server Error: ' + error?.data?.message || '') // General Error Message from Server.
   else formNode.setErrors('Error Connecting to Server! ' + error.name) // Error occurred, but server did not send any error status ... (Could Not Connect to server)
