41cacbc7191df223a98b4ee4a14430c91dd547ea
Nahid Ferdous <65497148+nahidnfr123@users.noreply.github.com>
2023-02-23 00:34:14 +0600

    Forms Update

---
+++ nuxt3/src/components/common/Notification.vue
@@ -0,0 +1,10 @@
+<template>
+  <Teleport to="body">
+    <div class="modal">
+      <p>Hello from the modal!</p>
+      <button>
+        Close
+      </button>
+    </div>
+  </Teleport>
+</template>
---
+++ nuxt3/src/composables/useNotification.js
@@ -0,0 +1,19 @@
+const options = {
+  position: 'bottom left',
+  duration: 2000,
+}
+
+const $notify = {
+  async success(message = '', options = options) {
+
+  },
+  async error(message = '', options = options) {
+
+  },
+  async info(message = '', options = options) {
+
+  },
+  async warning(message = '', options = options) {
+
+  }
+}
