6b662d7bcf11ef61bf74ea0f58498836a6e8cebf
Nahid Ferdous <65497148+nahidnfr123@users.noreply.github.com>
2023-03-01 14:12:36 +0600

    Vue 3 Starter Done

--- vue3/src/views/HomeView.vue
+++ vue3/src/views/HomeView.vue
@@ -1,32 +1,32 @@
 <script setup lang="ts">
 import Hero from "@/components/home/Hero.vue";
-import {useNotificationStore} from "@/components/notification/notification.store";
-
-const {setNotification} = useNotificationStore()
-
-function trigger() {
-  setNotification({
-    "type": "info",
-    "message": 'Hello',
-    "showIcon": true,
-    "dismiss": {
-      "manually": true,
-      "automatically": true
-    },
-    "duration": 3000,
-    "showDurationProgress": true,
-    "appearance": "light"
-  })
-}
+// import {useNotificationStore} from "@/components/notification/notification.store";
+//
+// const {setNotification} = useNotificationStore()
+//
+// function trigger() {
+//   setNotification({
+//     "type": "info",
+//     "message": 'Hello',
+//     "showIcon": true,
+//     "dismiss": {
+//       "manually": true,
+//       "automatically": true
+//     },
+//     "duration": 3000,
+//     "showDurationProgress": true,
+//     "appearance": "light"
+//   })
+// }
 </script>
 
 <template>
   <main>
     <Hero/>
 
-    <div class="text-center">
-      <button class="bg-pink-400 text-white rounded-lg p-3 px-5" @click="trigger">Toast</button>
-    </div>
+    <!--    <div class="text-center">-->
+    <!--      <button class="bg-pink-400 text-white rounded-lg p-3 px-5" @click="trigger">Toast</button>-->
+    <!--    </div>-->
   </main>
 </template>
 
