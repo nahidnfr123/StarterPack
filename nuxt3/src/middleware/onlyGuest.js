bc5a87e68bccdf3e77a9b892b9a67da8a30052c6
Nahid Ferdous <65497148+nahidnfr123@users.noreply.github.com>
2023-03-01 22:52:12 +0600

    Nuxt auth Improvement. Next-auth added to Next Js

--- nuxt3/src/middleware/onlyGuest.js
+++ nuxt3/src/middleware/onlyGuest.js
@@ -5,5 +5,5 @@ export default defineNuxtRouteMiddleware((to, from) => {
   const authStore = useAuthStore()
   const {isAuthenticated} = authStore
   // if (authStore.isLoggedIn) return abortNavigation() //Abort page navigation 404
-  if (isAuthenticated) return navigateTo('/') //Abort page navigation 404
+  if (isAuthenticated) return navigateTo('/') // redirect to home page ...
 })
--- nuxt3/src/middleware/requireAuth.js
+++ nuxt3/src/middleware/requireAuth.js
@@ -1,9 +1,11 @@
 import {useAuthStore} from "~/stores/auth";
 
 export default defineNuxtRouteMiddleware((to, from) => {
-  // if (process.server) return
   const authStore = useAuthStore()
   const {isAuthenticated} = authStore
-  // console.log(isAuthenticated)
-  if (!isAuthenticated) return navigateTo('/auth/login?next=' + to.fullPath)
+  if (process.server && !authStore.token) { /// IN server only check for token ....
+    return navigateTo('/auth/login?next=' + to.fullPath)
+  } else if (!isAuthenticated) { // while in client check for both token and user data ...
+    return navigateTo('/auth/login?next=' + to.fullPath)
+  }
 })
