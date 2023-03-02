665da0607fe23251d781414975ac9e2b18457073
Nahid Ferdous <65497148+nahidnfr123@users.noreply.github.com>
2023-03-02 21:47:12 +0600

    Login Form Updated

--- nextjs/src/pages/auth/login.tsx
+++ nextjs/src/pages/auth/login.tsx
@@ -1,4 +1,4 @@
-import {Formik, Field} from "formik";
+import {Formik, Field, Form} from "formik";
 import {
   Box,
   Button,
@@ -15,8 +15,28 @@ import TheHead from "@/components/common/TheHead";
 import {ReactElement} from "react";
 import AuthLayout from "@/layouts/auth";
 import {signIn, signOut} from "next-auth/react";
+import * as Yup from "yup";
 
 function Login() {
+  const initialValues = {
+    email: "",
+    password: "",
+    rememberMe: false
+  }
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+        .required('Email field is required.')
+        .email('Invalid email'),
+    password: Yup.string()
+        .required('Password field is required.')
+        .min(6, 'Minimum 6 characters!')
+        .max(60, 'Maximum 60 characters'),
+  });
+
+  function handelSubmit(values: Object) {
+    alert(JSON.stringify(values, null, 2));
+  }
 
   async function handelGoogleLogin() {
     await signIn('google', {callbackUrl: 'http://127.0.0.1:3000'})
@@ -33,27 +53,29 @@ function Login() {
         <Heading textAlign='center' pb='4' as='h2' size='lg'>Login</Heading>
 
         <Formik
-            initialValues={{
-              email: "",
-              password: "",
-              rememberMe: false
-            }}
-            onSubmit={(values) => {
-              alert(JSON.stringify(values, null, 2));
-            }}
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handelSubmit}
         >
-          {({handleSubmit, errors, touched}) => (
-              <form onSubmit={handleSubmit}>
+          {({
+              handleSubmit,
+              errors,
+              touched,
+              isSubmitting,
+            }) => (
+              <Form onSubmit={handleSubmit}>
                 <VStack spacing={4} align="flex-start">
-                  <FormControl>
+                  <FormControl isInvalid={!!errors.email && touched.email}>
                     <FormLabel htmlFor="email">Email</FormLabel>
                     <Field
                         as={Input}
+                        error={!!errors.email && touched.email}
                         id="email"
                         name="email"
                         type="email"
                         variant="filled"
                     />
+                    <FormErrorMessage>{errors.email}</FormErrorMessage>
                   </FormControl>
                   <FormControl isInvalid={!!errors.password && touched.password}>
                     <FormLabel htmlFor="password">Password</FormLabel>
@@ -83,11 +105,16 @@ function Login() {
                   >
                     Remember me?
                   </Field>
-                  <Button type="submit" colorScheme="purple" width="full">
+                  <Button
+                      type="submit"
+                      colorScheme="purple"
+                      width="full"
+                      disabled={isSubmitting}
+                  >
                     Login
                   </Button>
                 </VStack>
-              </form>
+              </Form>
           )}
         </Formik>
 
