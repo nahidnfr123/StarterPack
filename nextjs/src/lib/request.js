63cefdc7833473fd505b4cd8d1fa1726af4787bd
Nahid Ferdous <65497148+nahidnfr123@users.noreply.github.com>
2023-03-05 03:45:07 +0600

    Problem getting token

--- nextjs/src/lib/request.js
+++ nextjs/src/lib/request.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import {getCookie} from 'cookies-next';
 // axios.defaults.baseURL = process.env.API_URL || `http://127.0.0.1:8000/api/`
 // axios.defaults.withCredentials = true
 
