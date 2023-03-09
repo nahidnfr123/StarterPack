8f381738e2302a4e0f45167e65e0b69cfece1e26
Nahid Ferdous <65497148+nahidnfr123@users.noreply.github.com>
2023-03-09 17:46:20 +0600

    Vue 3 - Localstorage Removed, Cookie added

--- vue3/src/composables/useToken.js
+++ vue3/src/composables/useToken.js
@@ -1,6 +1,6 @@
 import Cookies from 'js-cookie'
 
-export default function accessToken(token, clearToken = false) {
+export default function accessToken(token = null, clearToken = false) {
   if (token) Cookies.set('token', token, {expires: 100, path: '', secure: true})
   if (clearToken) Cookies.remove('token')
   return Cookies.get('token')
