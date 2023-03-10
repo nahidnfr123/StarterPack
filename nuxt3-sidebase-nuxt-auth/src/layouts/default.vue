c5cb33d6e56d5a82521da46777606c72e7941989
Nahid Ferdous <65497148+nahidnfr123@users.noreply.github.com>
2023-03-10 18:41:04 +0600

    sidebase nuxt auth removed

--- nuxt3-sidebase-nuxt-auth/src/layouts/default.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <main class="min-h-screen">
-    <CommonTheHeader/>
-    <main class="min-h-[70vh] mb-10">
-      <slot/>
-    </main>
-    <CommonTheFooter/>
-  </main>
-</template>
--- nuxt3-sidebase-nuxt-auth/src/pages/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <Container>
-    <HomeHero/>
-  </Container>
-</template>
-
-<script setup>
-import Container from "~/components/common/Container.vue"
-</script>
